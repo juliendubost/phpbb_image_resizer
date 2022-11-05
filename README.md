d58d60e58912ba6d522a036ce51cb585f573b781
Julien Dubost <allankicker@gmail.com>
2022-11-05 14:41:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Install
+
+```
+poetry install 
+```
+
+# Usage
+
+```python
+usage: resizeall.py [-h] [--ignore-ext] path
+
+resize all images files found in a given folder
+
+positional arguments:
+  path          image file path or folder path containing images to resize
+
+optional arguments:
+  -h, --help    show this help message and exit
+  --ignore-ext  ignore file extensions, in this case, try to open all files and ignore if an exception occurs (phpbb file names does not include extensions)
+```
---
+++ resizeall.py
@@ -0,0 +1,70 @@
+import os
+import re
+import logging
+import glob
+import argparse
+import magic
+from PIL import Image
+from resizeimage import resizeimage
+
+
+LOG = logging.getLogger(__name__)
+CONTAIN_WIDTH = 1024
+CONTAIN_HEIGHT = 768
+
+
+def fetch_image_files(rootpath, filter_extension=True):
+
+    extension_regexp = "(?i)(jpe?g|png|gif|bmp)"
+    image_full_paths = []
+
+    for dirpath, dirs, filenames in os.walk(rootpath):
+        for filename in filenames:
+            if filter_extension:
+                search = re.search(extension_regexp, filename)
+                if search is not None:
+                    image_full_paths.append(os.path.join(dirpath, filename))
+            else:
+                image_full_paths.append(os.path.join(dirpath, filename))
+
+    return image_full_paths
+
+
+def resize_and_replace(filepath):
+
+    fd_img = open(filepath, "r+b")
+    try:
+        img = Image.open(fd_img)
+    except Image.UnidentifiedImageError:
+        return
+
+    width, height = img.size
+    if width > CONTAIN_WIDTH and height > CONTAIN_HEIGHT:
+        img = resizeimage.resize_thumbnail(img, (CONTAIN_WIDTH, CONTAIN_HEIGHT))
+        if not img.format:
+            img.format = magic.from_file(filepath, mime=True).split("/")[-1].upper()
+        img.save(filepath, format=img.format)
+        LOG.info(f"{filepath} resized")
+        fd_img.close()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="resize all images files found in a given folder"
+    )
+    parser.add_argument(
+        "path", help="image file path or folder path containing images to resize"
+    )
+    parser.add_argument(
+        "--ignore-ext",
+        action="store_true",
+        help="ignore file extensions, in this case, try to open all files and ignore if an exception occurs (phpbb file names does not include extensions)",
+    )
+    args = parser.parse_args()
+    if os.path.isdir(args.path):
+        filepaths = fetch_image_files(args.path, not args.ignore_ext)
+    else:
+        filepaths = [args.path]
+    for filepath in filepaths:
+        resize_and_replace(filepath)
